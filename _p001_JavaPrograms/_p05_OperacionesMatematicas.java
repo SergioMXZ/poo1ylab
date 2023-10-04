f1a6ddd83759eceb06835a546e3541f565dc4c04
SergioMXZ <sergchioz@gmail.com>
2023-10-03 21:52:40 -0600

    Actividad_2

---
+++ _p001_JavaPrograms/_p05_OperacionesMatematicas.java
@@ -0,0 +1,20 @@
+
+
+public class _p05_OperacionesMatematicas {
+    public static void main(String[] args) {
+        double x, y, suma, resta, mult, div, res, pot ;
+        x = 10.5; y = 2.5;
+        suma = x + y;
+        resta = x - y;
+        mult = x * y;
+        div = x / y;
+        res = x % y;
+        pot = Math.pow(x, y);
+
+        System.out.println(String.format("Los  numeros son: %.2f, %.2f", x , y));
+        System.out.println("\nEl resultado de las operaciones es el siguiente:\n");
+        System.out.println(String.format("Suma: %.2f\nResta: %.2f",suma ,resta));
+        System.out.println(String.format("Multiplicacion: %.2f\nDivision: %.2f", mult, div));
+        System.out.println(String.format("Residuo: %.2f\nPotencia: %.2f",res ,pot));
+    }
+}
